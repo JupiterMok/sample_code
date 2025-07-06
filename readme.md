6288770bd943e61253d71abff6846cb05dddee90
ashel <ashel@gmail.com>
2025-07-06 22:15:14 +0900

    delete 수정 및 mysqlTestModel.tableName

--- readme.md
+++ readme.md
@@ -5,3 +5,13 @@
 - update 기능에서 대상을 서치할 때, id뿐 아니라 testcol으로도 검색 가능하게 분리하기
 - 로그 뿌릴 때 데이터베이스 값 새로로 나오게 하기
 - if 문 대신 switch 문으로 바꾸기
+
+- 주의
+- queryAsncy는 보안에 취약함. 쓰지 말기.
+- 약자 쓰지 말기
+- 이름 지을 때 앞부분 맞추기
+
+- new 버전으로 바꾸기
+- 추가 수정사항 생각해오기
+- 처음에 텍스트 입력하는 방식이 아니라 리스트를 보여주고 고르는 방식으로 바꾸기
+- instance.logger.error(`error: that id not exist in ${mysqlserver.mysqlPool.tableName}`); 요거 제대로 작동하게 만들기
--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import { stdin as input, stdout as output } from 'node:process';
 
 import instance from './instance.js';
 import MariaTestModel from './models/test.mysql.js';
+import mysqlserver from './core/mysql.core.js';
 
 const rl = readline.createInterface({ input, output });
 
@@ -37,69 +38,131 @@ const database = async () => {
   const mysqlTestModel = new MariaTestModel();
   const conncet = await MariaTestModel.openConnectionAsync(); // conncet에 데이터베이스 연결을 저장
 
-  const basequery = 'SELECT * FROM testtable';
-  const list = await mysqlTestModel.queryAsync(conncet, basequery); // 전체 리스트 가져오기
-  instance.logger.info(JSON.stringify(list));
+  const list = await mysqlTestModel.findByFilterAsync(conncet); // 전체 리스트 가져오기
+  instance.logger.info(JSON.stringify(list, null, 2));
   // instance.logger.info(list.join('\n')); 요거 되게 만들어보기
 
-  const answer = await rl.question('\nplease enter IDUS, if you want exit enter blank '); // 분기 1 readline으로 문자열 받아와서 공백이면 종료하고 IDUS면 각각 맞는 명령어 실행
-  processEndCheck(answer);
-
   while (true) {
-    const insertValue = await rl.question('\nplease enter text ');
-    processEndCheck(insertValue);
+    const answer = await rl.question('\nplease enter IDUS, if you want exit enter blank '); // 분기 1 readline으로 문자열 받아와서 공백이면 종료하고 IDUS면 각각 맞는 명령어 실행
+    processEndCheck(answer);
+
     switch (answer) {
       case 'insert': {
-        // const insertValue = await rl.question(`\nplease enter insert value`);
-        const findOption = { testcol: insertValue };
-        const insertId = await mysqlTestModel.insertAsync(conncet, findOption);
-        const insertResult = await mysqlTestModel.findByFilterAsync(conncet, { id: insertId }); // 이거 한 줄로 줄일 수 없는지 확인하기. 윗 줄의 함수받아와서 insert한 목록 보여줌.
+        const getCliInputValue = await rl.question('\nplease enter text ');
+
+        const inputData = { testcol: getCliInputValue };
+
+        const getInsertId = await mysqlTestModel.insertAsync(conncet, inputData);
+
+        const insertDataId = { id: getInsertId };
+
+        const insertResult = await mysqlTestModel.findByFilterAsync(conncet, insertDataId); // 이거 한 줄로 줄일 수 없는지 확인하기. 윗 줄의 함수받아와서 insert한 목록 보여줌.
         instance.logger.info(JSON.stringify(insertResult));
         break;
       }
       case 'select': {
         const selectId = await rl.question(`\nplease enter id what you want select `);
-        const selectResult = await mysqlTestModel.findByFilterAsync(conncet, { id: selectId });
-        instance.logger.info(JSON.stringify(selectResult));
-        break;
-      }
-      case 'filter': {
-        const filteranswer = await rl.question('\nplease enter id what you want search ');
-        const filterResult = await mysqlTestModel.countByFilterAsync(conncet, { id: filteranswer });
-        instance.logger.info(JSON.stringify(filterResult));
-        break;
-      }
-      case 'update': {
-        const updataId = await rl.question('\nplease enter update filter ');
-        const updataText = await rl.question('\nplease enter update text ');
-        const updataBoolean = await mysqlTestModel.updateByFilterAsync(
-          conncet,
-          { id: updataId },
-          { testcol: updataText }
-        );
-        if (updataBoolean === true) {
-          const updataResult = await mysqlTestModel.findByFilterAsync(conncet, { id: updataId });
-          instance.logger.info(JSON.stringify(updataResult));
+        let selectResult;
+
+        if (selectId === '') {
+          selectResult = await mysqlTestModel.allAsync(conncet);
+        } else if (!Number.isInteger(Number(selectId))) {
+          instance.logger.error('error: id must be Number');
         } else {
-          instance.logger.error('error: id must be Number'); // 이거 오류 메세지 id가 숫자가 아님이랑 대상 id를 가진 값이 존재하지 않습니다. 둘로 나눠야 함
+          const inputData = { id: selectId };
+
+          selectResult = await mysqlTestModel.findByFilterAsync(conncet, inputData);
+
+          instance.logger.info(JSON.stringify(selectResult, null, 2));
         }
         break;
       }
-      case 'delete': {
-        const deleteId = await rl.question('\nplease enter delete id ');
-        const deleteBoolean = await mysqlTestModel.deleteByFilterAsync(conncet, { id: deleteId });
-        if (deleteBoolean === true) {
-          const deleteResult = await mysqlTestModel.findByFilterAsync(conncet, { id: deleteId });
-          instance.logger.info(JSON.stringify(deleteResult));
-        } else {
+      case 'selectnew': {
+        const selectId = await rl.question(`\nplease enter id what you want select `);
+        if (!Number.isInteger(Number(selectId))) {
           instance.logger.error('error: id must be Number');
-        } // 이거 오류 메세지 id가 숫자가 아님이랑 대상 id를 가진 값이 존재하지 않습니다. 둘로 나눠야 함
+          break;
+        }
+
+        let inputData = {};
+        if (selectId !== '') {
+          inputData = { id: selectId };
+        }
+
+        const selectResult = await mysqlTestModel.findByFilterAsync(conncet, inputData);
+
+        instance.logger.info(JSON.stringify(selectResult, null, 2));
         break;
       }
+
+      case 'count': {
+        const getSearchFilter = await rl.question('\nplease enter id what you want search ');
+
+        if (!Number.isInteger(Number(getSearchFilter))) {
+          instance.logger.error('error: id must be Number');
+        }
+
+        const searchIdfilter = { id: getSearchFilter };
+
+        const filterResult = await mysqlTestModel.countByFilterAsync(conncet, searchIdfilter);
+
+        instance.logger.info(JSON.stringify(filterResult));
+        break;
+      }
+
+      case 'update': {
+        const getUpdateId_Text = await rl.question('\nplease enter update filter and text ');
+        // const updateText = await rl.question('\nplease enter update text ');
+
+        const upDateData = getUpdateId_Text.split(' ');
+
+        if (!Number.isInteger(Number(upDateData[0]))) {
+          instance.logger.error('error: id must be Number');
+          break;
+        }
+
+        const findDataId = { id: upDateData[0] };
+        const toUpdateData = { testcol: upDateData[1] };
+
+        const upDateBoolean = await mysqlTestModel.updateByFilterAsync(conncet, findDataId, toUpdateData);
+
+        if (upDateBoolean === false) {
+          instance.logger.error(`error: that id not exist in ${mysqlTestModel.tableName}`);
+          break;
+        }
+
+        const updataResult = await mysqlTestModel.findByFilterAsync(conncet, findDataId); // 요걸 생략할 순 없을까?
+        instance.logger.info(JSON.stringify(updataResult));
+
+        break;
+      }
+
+      case 'delete': {
+        const deleteId = await rl.question('\nplease enter delete id ');
+
+        if (!Number.isInteger(Number(deleteId))) {
+          instance.logger.error('error: id must be Number');
+        }
+
+        const findDeleteId = { id: deleteId };
+
+        const deleteBoolean = await mysqlTestModel.deleteByFilterAsync(conncet, findDeleteId);
+
+        if (deleteBoolean === false) {
+          instance.logger.error(`error: that id not exist in ${mysqlTestModel.tableName}`);
+          break;
+        }
+
+        const deleteResult = await mysqlTestModel.findByFilterAsync(conncet, findDeleteId);
+        instance.logger.info(JSON.stringify(deleteResult));
+
+        break;
+      }
+
       default:
-        instance.logger.error(`text error: answer is not a IUSD`);
-    }
-  }
-};
+        instance.logger.error(`text error: answer is not a insert, update, select, delete, count`);
+    } // swich 문 중괄호
+  } // while 문 중괄호
+}; // funtion 선언 중괄호
 
 database();
