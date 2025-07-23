e9ee49e374daf3839de9e1213f9e46d7e6793afc
ashel <ashel@gmail.com>
2025-07-23 21:20:50 +0900

    table 분할 코드 간략화 중간 저장

--- src/router.js
+++ src/router.js
@@ -2,8 +2,17 @@ import toolRoute0 from './routers/tool_table_0.js';
 import toolRoute1 from './routers/tool_table_1.js';
 import toolRoute2 from './routers/tool_table_2.js';
 
+let routerLine;
 export default (app) => {
-  app.use('/routers0/tool', toolRoute0);
-  app.use('/routers1/tool', toolRoute1);
-  app.use('/routers2/tool', toolRoute2);
+  app.use('/table0/tool', toolRoute0, () => {
+    routerLine = 0;
+  });
+  app.use('/table1/tool', toolRoute1, () => {
+    routerLine = 1;
+  });
+  app.use('/table2/tool', toolRoute2, () => {
+    routerLine = 2;
+  });
 };
+
+export const tableNumber = routerLine;
--- src/routers/tool_table_0.js
+++ src/routers/tool_table_0.js
@@ -1,10 +1,49 @@
 import express, { Router } from 'express';
 
 import instance from '../instance.js';
-import MariaTestModel from '../models/test.mysql_0.js';
+import MariaTestModel0 from '../models/test.mysql_0.js';
+import MariaTestModel1 from '../models/test.mysql_1.js';
+import MariaTestModel2 from '../models/test.mysql_2.js';
 import mysqlserver from '../core/mysql.core.js';
+// import tableNumber from '../router';
 
 const router = express.Router();
+const app = express();
+async function selectTable() {
+  let mysqlTestModel;
+  if (tableNumber === 0) {
+    mysqlTestModel = new MariaTestModel0();
+  } else if (tableNumber === 1) {
+    mysqlTestModel = new MariaTestModel1();
+  } else {
+    mysqlTestModel = new MariaTestModel2();
+  }
+  return mysqlTestModel;
+}
+
+async function connectDb() {
+  let connect;
+  if (tableNumber === 0) {
+    connect = await MariaTestModel0.openConnectionAsync();
+  } else if (tableNumber === 1) {
+    connect = await MariaTestModel1.openConnectionAsync();
+  } else {
+    connect = await MariaTestModel2.openConnectionAsync();
+  }
+  return connect;
+}
+
+let tableNumber;
+
+app.use('/table0/tool/', (req, res, next) => {
+  tableNumber = 0;
+});
+app.use('/table1/tool', (req, res, next) => {
+  tableNumber = 1;
+});
+app.use('/table2/tool', (req, res, next) => {
+  tableNumber = 2;
+});
 
 router
   .get('/', (req, res) => {
@@ -13,20 +52,27 @@ router
   })
   .get('/select', async (req, res) => {
     const { id } = req.query;
-    const connect = await MariaTestModel.openConnectionAsync();
-    const mysqlTestModel = new MariaTestModel();
+
+    // const mysqlTestModel = selectTable();
+    // const connect = connectDb();
+
+    const connect = await MariaTestModel0.openConnectionAsync();
+    const mysqlTestModel = new MariaTestModel0();
+
     const result = await mysqlTestModel.findByFilterAsync(connect, { id });
 
     // await mysqlTestModel.closeConnectionAsync(connect);
 
     res.json(result);
-  })
-  .get('/insert/:text', async (req, res) => {
-    const connect = await MariaTestModel.openConnectionAsync();
-    const mysqlTestModel = new MariaTestModel();
+  });
 
-    const insertdata = { testcol: req.text };
-    const index = await mysqlTestModel.insertAsync(connect, insertdata);
+router
+  .post('/insert', async (req, res) => {
+    const connect = await MariaTestModel0.openConnectionAsync();
+    const mysqlTestModel = new MariaTestModel0();
+
+    const testcol = { testcol: req.body.testcol };
+    const index = await mysqlTestModel.insertAsync(connect, testcol);
 
     const resreachFilter = { id: index };
     const result = await mysqlTestModel.findByFilterAsync(connect, resreachFilter);
@@ -34,12 +80,10 @@ router
     // await mysqlTestModel.closeConnectionAsync(connect); 함수가 정의되지 않음
 
     res.json(result);
-  });
-
-router
+  })
   .post('/update', async (req, res) => {
-    const connect = await MariaTestModel.openConnectionAsync();
-    const mysqlTestModel = new MariaTestModel();
+    const connect = await MariaTestModel0.openConnectionAsync();
+    const mysqlTestModel = new MariaTestModel0();
 
     const filter = { id: req.body.id };
     const updateData = { testcol: req.body.testcol };
@@ -56,8 +100,8 @@ router
     res.json(message);
   })
   .post('/delete', async (req, res) => {
-    const connect = await MariaTestModel.openConnectionAsync();
-    const mysqlTestModel = new MariaTestModel();
+    const connect = await MariaTestModel0.openConnectionAsync();
+    const mysqlTestModel = new MariaTestModel0();
 
     const result = await mysqlTestModel.deleteByFilterAsync(connect, req.body); // 이것도 object로 선언해주는 게 좋나?
 
