ff651e5a1b95a7facd041bdc887551928481e7e5
ashel <ashel@gmail.com>
2025-07-23 19:03:51 +0900

    router main.js 밖으로 뺌

--- src/main.js
+++ src/main.js
@@ -9,67 +9,13 @@ import MariaTestModel from './models/test.mysql.js';
 import mysqlserver from './core/mysql.core.js';
 import database from './models/datebase.js';
 import serverDatabase from './models/serverdatadase.js';
+import router from './models/router.js';
 
 const app = express();
-// const router = express.Router();
 const port = 3000;
-
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  // res.send('This is mysql test pge');
-  const message = { message: 'This is mysql test page' };
-  res.json(message);
-});
-
-app.get('/tool/select', async (req, res) => {
-  const connect = await MariaTestModel.openConnectionAsync();
-  const mysqlTestModel = new MariaTestModel();
-  const result = await mysqlTestModel.findByFilterAsync(connect, req.query);
-  // await mysqlTestModel.closeConnectionAsync(connect); 함수가 정의되지 않음
-  res.json(result);
-});
-
-app.post('/tool/insert', async (req, res) => {
-  const connect = await MariaTestModel.openConnectionAsync();
-  const mysqlTestModel = new MariaTestModel();
-  const index = await mysqlTestModel.insertAsync(connect, req.body);
-  const resreachFilter = { id: index };
-  const result = await mysqlTestModel.findByFilterAsync(connect, resreachFilter);
-  // await mysqlTestModel.closeConnectionAsync(connect); 함수가 정의되지 않음
-  res.json(result);
-});
-
-// const result = await axios.post('localhost:3000/tool/insert/7', {
-// result.id, result.name[3]
-// testcol: 'nomad', // post 로 보낼 데이터
-// });
-
-app.post('/tool/update', async (req, res) => {
-  const connect = await MariaTestModel.openConnectionAsync();
-  const mysqlTestModel = new MariaTestModel();
-  const filter = { id: req.body.id };
-  const updateData = { testcol: req.body.testcol };
-  const result = await mysqlTestModel.updateByFilterAsync(connect, filter, updateData);
-  // await mysqlTestModel.closeConnectionAsync(connect); 함수가 정의되지 않음
-  let message = { message: 'update is fail' };
-  if (result === true) {
-    message = { message: 'update is succeed' };
-  }
-  res.json(message);
-});
-
-app.post('/tool/delete', async (req, res) => {
-  const connect = await MariaTestModel.openConnectionAsync();
-  const mysqlTestModel = new MariaTestModel();
-  const result = await mysqlTestModel.deleteByFilterAsync(connect, req.body);
-  // await mysqlTestModel.closeConnectionAsync(connect); 함수가 정의되지 않음
-  let message = { message: 'delete is fail' };
-  if (result === true) {
-    message = { message: 'delete is succeed' };
-  }
-  res.json(message);
-});
+app.use('/tool', router);
 
 app.listen(port, () => {
   instance.logger.info(`Example app listening on port ${port}`);
--- src/models/router.js
+++ src/models/router.js
@@ -4,69 +4,68 @@ import instance from '../instance.js';
 import MariaTestModel from '../models/test.mysql.js';
 import mysqlserver from '../core/mysql.core.js';
 
-// 이걸 함수로 내보내야 하나?
-const app = express();
-// const router = express.Router();
-const port = 3000;
+const router = express.Router();
 
-app.use(express.json());
+router
+  .get('/', (req, res) => {
+    const message = { message: 'This is mysql test page' };
+    res.json(message);
+  })
+  .get('/select/:id', async (req, res) => {
+    const connect = await MariaTestModel.openConnectionAsync();
+    const mysqlTestModel = new MariaTestModel();
+    const result = await mysqlTestModel.findByFilterAsync(connect, req.id);
 
-app.get('/', (req, res) => {
-  // res.send('This is mysql test pge');
-  const message = { message: 'This is mysql test page' };
-  res.json(message);
-});
+    // await mysqlTestModel.closeConnectionAsync(connect); 함수가 정의되지 않음
 
-app.get('/tool/select', async (req, res) => {
-  const connect = await MariaTestModel.openConnectionAsync();
-  const mysqlTestModel = new MariaTestModel();
-  const result = await mysqlTestModel.findByFilterAsync(connect, req.query);
-  // await mysqlTestModel.closeConnectionAsync(connect); 함수가 정의되지 않음
-  res.json(result);
-});
+    res.json(result);
+  })
+  .get('/insert/:text', async (req, res) => {
+    const connect = await MariaTestModel.openConnectionAsync();
+    const mysqlTestModel = new MariaTestModel();
 
-app.post('/tool/insert', async (req, res) => {
-  const connect = await MariaTestModel.openConnectionAsync();
-  const mysqlTestModel = new MariaTestModel();
-  const index = await mysqlTestModel.insertAsync(connect, req.body);
-  const resreachFilter = { id: index };
-  const result = await mysqlTestModel.findByFilterAsync(connect, resreachFilter);
-  // await mysqlTestModel.closeConnectionAsync(connect); 함수가 정의되지 않음
-  res.json(result);
-});
+    const insertdata = { testcol: req.text };
+    const index = await mysqlTestModel.insertAsync(connect, insertdata);
 
-// const result = await axios.post('localhost:3000/tool/insert/7', {
-// result.id, result.name[3]
-// testcol: 'nomad', // post 로 보낼 데이터
-// });
+    const resreachFilter = { id: index };
+    const result = await mysqlTestModel.findByFilterAsync(connect, resreachFilter);
 
-app.post('/tool/update', async (req, res) => {
-  const connect = await MariaTestModel.openConnectionAsync();
-  const mysqlTestModel = new MariaTestModel();
-  const filter = { id: req.body.id };
-  const updateData = { testcol: req.body.testcol };
-  const result = await mysqlTestModel.updateByFilterAsync(connect, filter, updateData);
-  // await mysqlTestModel.closeConnectionAsync(connect); 함수가 정의되지 않음
-  let message = { message: 'update is fail' };
-  if (result === true) {
-    message = { message: 'update is succeed' };
-  }
-  res.json(message);
-});
+    // await mysqlTestModel.closeConnectionAsync(connect); 함수가 정의되지 않음
 
-app.post('/tool/delete', async (req, res) => {
-  const connect = await MariaTestModel.openConnectionAsync();
-  const mysqlTestModel = new MariaTestModel();
-  const result = await mysqlTestModel.deleteByFilterAsync(connect, req.body);
-  // await mysqlTestModel.closeConnectionAsync(connect); 함수가 정의되지 않음
-  let message = { message: 'delete is fail' };
-  if (result === true) {
-    message = { message: 'delete is succeed' };
-  }
-  res.json(message);
-});
+    res.json(result);
+  });
 
-app.listen(port, () => {
-  instance.logger.info(`Example app listening on port ${port}`);
-});
-export default Router;
+router
+  .post('/update', async (req, res) => {
+    const connect = await MariaTestModel.openConnectionAsync();
+    const mysqlTestModel = new MariaTestModel();
+
+    const filter = { id: req.body.id };
+    const updateData = { testcol: req.body.testcol };
+
+    const result = await mysqlTestModel.updateByFilterAsync(connect, filter, updateData);
+
+    // await mysqlTestModel.closeConnectionAsync(connect); 함수가 정의되지 않음
+
+    let message = { message: 'update is fail' };
+    if (result === true) {
+      message = { message: 'update is succeed' };
+    }
+
+    res.json(message);
+  })
+  .post('/delete', async (req, res) => {
+    const connect = await MariaTestModel.openConnectionAsync();
+    const mysqlTestModel = new MariaTestModel();
+
+    const result = await mysqlTestModel.deleteByFilterAsync(connect, req.body); // 이것도 object로 선언해주는 게 좋나?
+
+    // await mysqlTestModel.closeConnectionAsync(connect); 함수가 정의되지 않음
+
+    let message = { message: 'delete is fail' };
+    if (result === true) {
+      message = { message: 'delete is succeed' };
+    }
+    res.json(message);
+  });
+export default router;
